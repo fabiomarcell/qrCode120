2c7aa370a9679d178fcf6d39ec7cf125fdb15a0a
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-16 03:28:09 -0200

    test bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
 <?php
 ini_set("display_errors",1);
 include('phpqrcode/qrlib.php');
@@ -10,8 +12,9 @@ while($x <= 120){
 
 $x = 1;
 while($x <= 120){
-	echo '<img src="QR_code'.$x.'.png">';
+	echo '<div class="col-md-3><img src="QR_code'.$x.'.png">';
 	echo $x;
+	echo '</div>';
 	$x++;
 }
 ?>
