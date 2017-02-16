caecd19881d86ecfdcd8e9c298abc91c7e772e87
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-16 03:26:35 -0200

    fix loop

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ ini_set("display_errors",1);
 include('phpqrcode/qrlib.php');
 
 $x = 1;
-while($x != 120){
+while($x <= 120){
 	QRcode::png("Seu Número da Sorte: ".$x, "QR_code".$x.".png", QR_ECLEVEL_H , 4); 
 	$x++;
 }
 
 $x = 1;
-while($x != 120){
+while($x <= 120){
 	echo '<img src="QR_code'.$x.'.png">';
 	echo $x;
 	$x++;
