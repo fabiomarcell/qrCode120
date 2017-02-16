c99f8e9d929afb5d80a992089fac007821faeb2b
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-16 03:17:31 -0200

    Teste de qrcode com 120 registros

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 ini_set("display_errors",1);
 include('phpqrcode/qrlib.php');
-QRcode::png("http://www.botecodigital.info", "QR_code.png", QR_ECLEVEL_H , 4); 
+
+$x = 1;
+while(x <= 120){
+	QRcode::png("Seu Número da Sorte: ".$x, "QR_code".$x.".png", QR_ECLEVEL_H , 4); 
+	$x++;
+}
+
+$x = 1;
+while(x <= 120){
+	echo '<img src="QR_code'.$x.'.png">';
+	$x++;
+}
 ?>
-<img src="QR_code.png">
