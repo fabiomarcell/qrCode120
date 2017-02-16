3339da47c6a29a193eaa00528b3d9ef6caa357f3
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-16 08:15:03 -0200

    fix bootstrap

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo "<div class='container'><div class='row'>";
 while($x <= 120){
 	QRcode::png("Seu Número da Sorte: ".$x, "QR_code".$x.".png", QR_ECLEVEL_H , 4); 
 
-	echo '<div class="col-md-3"><img src="QR_code'.$x.'.png" style="text-align:center;">';
+	echo '<div class="col-md-3" style="text-align:center;"><img src="QR_code'.$x.'.png">';
 	echo "<br>";
 	echo "<p>Utilize um leitor de QRCode para saber seu número da sorte! =)</p>";
 	echo '</div>';
